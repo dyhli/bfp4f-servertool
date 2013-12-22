157a1c0ab023b7b06315c5f6327a6b719d9c627f
dyhli <kick_yu_gi_oh@hotmail.com>
2013-12-22 23:05:43 +0100

    Small fix

--- core/cp_footer.php
+++ core/cp_footer.php
@@ -1,7 +1,7 @@
 			<footer class="row">
 				<div class="col-md-12">
 					<hr />		
-					<p class="text-muted"><a href="http://battlefieldtools.com" target="_blank"><i class="fa fa-wrench"></i> BattlefieldTools.com</a> &middot; <a href="http://battlefield.play4free.com/en/forum/showthread.php?tid=137006" target="_blank"><i class="fa fa-file-text"></i> <?=$lang['cp_footer_thread']?></a> &middot; <a href="https://github.com/dyhli/bfp4f-servertool/" target="_blank"><i class="fa fa-github"></i> <?=$lang['github']?></a> <span class="pull-right">BattlefieldTools Servertool is licensed under GPL v3.0.<br />Copyright &copy; <?=date('Y')?> by Danny Li <SharpBunny></span></p>
+					<p class="text-muted"><a href="http://battlefieldtools.com" target="_blank"><i class="fa fa-wrench"></i> BattlefieldTools.com</a> &middot; <a href="http://battlefield.play4free.com/en/forum/showthread.php?tid=137006" target="_blank"><i class="fa fa-file-text"></i> <?=$lang['cp_footer_thread']?></a> &middot; <a href="https://github.com/dyhli/bfp4f-servertool/" target="_blank"><i class="fa fa-github"></i> <?=$lang['github']?></a> <span class="pull-right">BattlefieldTools Servertool is licensed under GPL v3.0.<br />Copyright &copy; <?=date('Y')?> by Danny Li &lt;SharpBunny&gt;</span></p>
 				</div>
 			</footer>
 			
